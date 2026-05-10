c71076d6ba53f5e3b41d642582d6fe63a988ecfc
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-05-10 22:12:51 +0530

    LCM using relation of HCF AND LCM

--- src/LCM.java
+++ src/LCM.java
@@ -1,5 +1,6 @@
 public class LCM {
-    static int lcm(int a,int b){
+    // First Method ....
+   /* static int lcm(int a,int b){
         if(a==b)return a;
         int large = Math.max(a,b);
         int small = Math.min(a,b);
@@ -7,9 +8,17 @@ public class LCM {
             if(i%small==0) return i;
         }
         return large*small;
+    }*/
+
+    // Method Second by using relation of LCM or HCF...
+    static int gcd(int a, int b){
+        return(b==0) ? a : gcd(b,a%b);
+    }
+    static int lcm(int a,int b){
+        return (a/gcd(a,b))*b;
     }
     public static void main(String[] args) {
-        System.out.println(lcm(3,31));
+        System.out.println(lcm(16,28));
 
     }
 }
