47e0156d909899b915bad2d8d6bce12da07a0e25
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-05-16 20:35:02 +0530

    Reverse Array character

---
+++ src/ReverseArrayString.java
@@ -0,0 +1,20 @@
+public class ReverseArrayString {
+    static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'h', 'l', 'u', 'm'};
+        reverseString(s);
+    }
+}
