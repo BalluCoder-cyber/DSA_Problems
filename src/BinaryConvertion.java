6640c975404ec9fd5759e1af1aec8d6d66f16347
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-05-19 23:06:41 +0530

    Integer to binary

---
+++ src/BinaryConvertion.java
@@ -0,0 +1,16 @@
+public class BinaryConvertion {
+    static String intTOBinary(int n) {
+        if (n == 1) return "1";
+        String binary = " ";
+        while (n > 0) {
+            binary = (n%2) + binary;
+            n = n / 2;
+        }
+        return binary;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intTOBinary(2));
+
+    }
+}
