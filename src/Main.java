f08cb16a77401c6f7d25dced07396a1e8e895d96
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-05-24 11:10:48 +0530

    Find_IndexSortedArr of multi time same occurrence

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
+    public static String searchRange(int[] nums, int target) {
+        int first = -1, last = -1;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == target) {
+//                if (first == -1) {
+//                    first = i;
+//                }
+//                last = i;
+//            }
+//        }
+        int i = 0;
+        int j = nums.length-1;
+        while (i<=j){
+            if (nums[i] == target) {
+                if (first == -1) {
+                    first = i;
+                }
+                last = i;
+            }
+            i++;
+        }
+        return Arrays.toString(new int[]{first, last}) ;
+
+    }
     public static void main(String[] args) {
-      int number = 10;
-        System.out.println(++number + number++);
+        int[] nums = {5,7,7,3,4,10};
+        System.out.println(searchRange(nums,7));
+
     }
 }
