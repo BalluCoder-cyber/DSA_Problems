dce927369960deb64caf2378413b29f7369f907a
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-05-31 22:29:04 +0530

    Majority value in array

---
+++ src/MajorityElement.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int n = nums.length / 2;
+        int maxCount = Integer.MIN_VALUE;
+        Arrays.sort(nums);
+
+        return nums[n+1];
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {3,2,3};
+        System.out.println(MajorityElement.majorityElement(arr));
+    }
+}
