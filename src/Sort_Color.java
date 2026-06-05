af3198c28644d2adfb07405f06ae666bc8a22adf
BalluCoder-cyber <balwantahirwar70@gmail.com>
2026-06-06 00:11:57 +0530

    Sort color

---
+++ src/Sort_Color.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Sort_Color {
+    public static void sortColors(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0; j<nums.length-1;j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+    public static void main(String[] args) {
+       int[] num = {2,0,2,1,1,0};
+       sortColors(num);
+    }
+}
